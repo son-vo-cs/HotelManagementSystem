ebe9f5a3d7fea3d190430c5702590ebab6f1ba34
Aldo Gutierrez <aldodario1@hotmail.es>
2019-07-20 21:48:35 -0700

    Initialized connection driver for JDBC connector

---
+++ src/ConnectionDriver.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+import java.io.*
+import java.util.Scanner;
+
+public class ConnectionDriver
+{
+    private String password;
+    
+    //TODO
+    public static Connection getConnection()
+    {
+	return null;
+    }
+}
